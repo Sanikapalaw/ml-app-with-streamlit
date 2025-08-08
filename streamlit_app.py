ec8838df1f4c1c82eba578b936b8241732b09ba5
Sanika Palaw <146212650+Sanikapalaw@users.noreply.github.com>
2025-08-08 08:49:41 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Machine Learning App")
+st.title("Machine learning app")
 
-st.info("This is a app build on ml models")
+st.info("This app is build using ml")
+
+df=pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+df
