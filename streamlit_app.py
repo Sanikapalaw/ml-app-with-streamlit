df4fc8cfae250695346d536b338e8fb2a65d0341
Sanika Palaw <146212650+Sanikapalaw@users.noreply.github.com>
2025-08-08 08:51:06 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title("Machine learning app")
 
 st.info("This app is build using ml")
 
-df=pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
-df
+with st.expander("Data"):
+  st.write("**Raw data**")
+  df=pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+  df
