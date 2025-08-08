8519502c76dbe3a43b9fe501060b83d477ba0dd5
Sanika Palaw <146212650+Sanikapalaw@users.noreply.github.com>
2025-08-08 09:00:24 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ with st.expander("Data"):
   y=df.species
   y
 
-write st.expander("Data Visualization"):
+with st.expander("Data Visualization"):
   st.scatter_chart(data=df,x="bill_length_mm",y='body_mass_g',color="species")
 
   
