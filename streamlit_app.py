82275b97c455a5cb768afb843e92ceaa7d60cefc
Sanika Palaw <146212650+Sanikapalaw@users.noreply.github.com>
2025-08-08 09:08:39 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,4 +21,14 @@ with st.expander("Data"):
 with st.expander("Data Visualization"):
   st.scatter_chart(data=df,x="bill_length_mm",y='body_mass_g',color="species")
 
+
+#Data  Preparation
+
+with st.sidebar:
+  st.header("Input Features")
+  #"species","island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
+  island=st.selectbox('Island',('Biscoe','Dream','Torgersen'))
+  island=st.selectbox('Gender',('Gender',('male','female'))
+  bill_length_mm=st.slider('Bill length (mm)',32.1,59.6,43.9)
   
+                   
